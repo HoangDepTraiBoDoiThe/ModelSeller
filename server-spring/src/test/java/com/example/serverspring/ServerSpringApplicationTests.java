13c94ff381a34bf633325544047acdc391ecb586
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-10-05 22:55:59 +0700

    Init project

---
+++ server-spring/src/test/java/com/example/serverspring/ServerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
